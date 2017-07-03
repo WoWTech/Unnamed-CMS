0be05b2c093a2d82deba39ce7f0abc23fbf53c0e
Tech Gen <tech.gen@mail.ru>
2017-07-03 15:46:18 +0300

    Caching online players

--- app/Services/Server.php
+++ app/Services/Server.php
@@ -20,6 +20,16 @@ class Server
         return $status;
     }
 
+    public static function playersOnline()
+    {
+        $online = Cache::remember('players_online', 0.1 , function () {
+            return static::getOnlinePlayers();
+        });
+
+        return $online;
+    }
+
+
     // For now it's hardcoded, because the only template we got
     // at the moment supports only 1 realm.
 
@@ -32,11 +42,11 @@ class Server
 
         return (object) array('status' => $result, 'name' => $realms[0]['name'], 'port' => $realms[0]['port'], 'ip' => $realms[0]['ip']);
     }
-
-    public static function playersOnline()
+    
+    private static function getOnlinePlayers()
     {
         $playersOnline = DB::connection('characters')->table('characters')->where('online', 0)->get(['name', 'race', 'class', 'level']);
-        
+
         $allianceOnline = $playersOnline->whereIn('race', [1,3,4,7,11])->count();
         $hordeOnline    = $playersOnline->whereIn('race', [2,5,6,8,10])->count();
 
