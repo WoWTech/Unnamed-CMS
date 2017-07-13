b075d5c84e005f21e908e24acb6aa100633748b4
Tech Gen <tech.gen@mail.ru>
2017-07-13 15:28:18 +0300

    Implemented main dashboard page

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,98 @@
+@extends('layouts.dashboard')
+
+@section('content')
+  <header>
+    <h2>Dashboard</h2>
+  </header>
+
+  <div class="content">
+
+    <div class="card">
+      <header>
+        New posts
+      </header>
+
+      <div class="card-body">
+        <table>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>User</th>
+              <th>Date</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach($posts as $post)
+              <tr>
+                <td>{{ $post->title }}</td>
+                <td>{{ $post->account->username }}</td>
+                <td>{{ $post->created_at->toFormattedDateString() }}</td>
+                <td><a href="#">Edit</a> <a href="#">Delete</a></td>
+              </tr>
+            @endforeach
+          </tbody>
+        </table>
+      </div>
+    </div>
+
+    <div class="card">
+      <header>
+        New comments
+      </header>
+
+      <div class="card-body">
+        <table>
+          <thead>
+            <tr>
+              <th>Comment</th>
+              <th>User</th>
+              <th>Date</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach($comments as $comment)
+              <tr>
+                <td>{{ $comment->content }}</td>
+                <td>{{ $comment->account->username }}</td>
+                <td>{{ $comment->created_at->toFormattedDateString() }}</td>
+                <td><a href="#">Edit</a> <a href="#">Delete</a></td>
+              </tr>
+            @endforeach
+          </tbody>
+        </table>
+      </div>
+    </div>
+
+    <div class="card">
+      <header>
+        Registered users
+      </header>
+
+      <div class="card-body">
+        <table>
+          <thead>
+            <tr>
+              <th>Username</th>
+              <th>Email</th>
+              <th>Date</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            @foreach($users as $user)
+              <tr>
+                <td>{{ $user->username }}</td>
+                <td>{{ $user->email }}</td>
+                <td>{{ $user->joindate->toFormattedDateString() }}</td>
+                <td><a href="#">Edit</a> <a href="#">Delete</a></td>
+              </tr>
+            @endforeach
+          </tbody>
+        </table>
+      </div>
+    </div>
+
+  </div>
+@endsection
---
+++ resources/views/layouts/dashboard-sidebar.blade.php
@@ -0,0 +1,8 @@
+<aside>
+  <ul class="side-menu">
+    <li><a href="{{ route('dashboard') }}" class="active">Dashboard</a></li>
+    <li><a href="#">Posts</a></li>
+    <li><a href="#">Comments</a></li>
+    <li><a href="#">Permissions</a></li>
+  </ul>
+</aside>
