59546d2e13ea773b74d194fa9aedd6505916c725
Tech Gen <tech.gen@mail.ru>
2017-07-27 00:56:43 +0300

    Fixed autocomplete bug with different fields

--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -7,6 +7,9 @@
 
   <div class="content-wrapper">
     <form action="{{ route('admin.posts.update', $post) }}" method="POST">
+
+      @include('layouts.input_errors')
+
       {{ csrf_field() }}
       {{ method_field('PATCH') }}
       <div class="input-group">
@@ -19,7 +22,7 @@
       </div>
       <div class="input-group">
         <label for="author">Post author</label>
-        <input id="username" type="text" name="username" value="{{ $post->account->username }}" required>
+        <input id="username-autocomplete" type="text" name="username" value="{{ $post->account->username }}" required>
         <input id="acc" type="hidden" name="account_id" value="{{ $post->account->id }}" required>
       </div>
       <div class="input-group">
