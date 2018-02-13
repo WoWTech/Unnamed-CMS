f0751f3c02db85342b8bb3ebb6a804fc5561f4fc
WoWTech <wow.tech.gen@gmail.com>
2018-02-13 21:05:08 +0200

    Extracted and rewriten to pure JS method-links functionality
    Co-Authored-By: Volodymyr <dev.volodymyr@gmail.com>

---
+++ public/js/method-links.js
@@ -0,0 +1,40 @@
+const method_link = event => {
+  event.preventDefault();
+
+  const element = event.target;
+  const { method } = element.dataset;
+  const href = element.getAttribute('href');
+  const token = element.getAttribute('content');
+  const form = document.createElement('form');
+
+  form.setAttribute('action', href);
+  // This one is important even in case of 
+  // DELETE, PATCH, PUT and others method.
+  form.setAttribute('method', 'POST');
+
+  const _method = createInput('_method', 'hidden', method);
+  const _token = createInput('_token', 'hidden', token);
+
+  form.appendChild(_token);
+  form.appendChild(_method);
+
+  document.body.appendChild(form);
+
+  form.submit();
+};
+
+const createInput = (name, type, value) => {
+  let input = docment.createElement('input');
+
+  input.setAttribute('name', name);
+  input.setAttribute('type', type);
+  input.setAttribute('value', value);
+
+  return input;
+}
+
+const elements = document.querySelectorAll('a.method-link');
+
+for (let element of elements) {
+  element.onlick = method_link;
+}
