04e85e58c93b7b8673bc43cad605d20f6422db5b
WoWTech <wow.tech.gen@gmail.com>
2018-03-07 00:10:48 +0200

    Redux setup with devTools web plugin
    Co-Authored-By: Volodymyr <dev.volodymyr@gmail.com>

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -2,14 +2,30 @@ require('./bootstrap');
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunk from 'redux-thunk';
+
+import entities from './reducers/entities.js';
+import pagination from './reducers/posts_pagination.js';
 
 import App from './components/App.js';
 import Forum from './components/forum/Forum.js';
 
+const rootReducer = combineReducers({
+  entities,
+  pagination
+})
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+let store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+
 render(
-  <Router>
-    <Switch>
-      <Route path='/forum' component={Forum} />
-      <Route path='/' component={App} />
-    </Switch>
-  </Router>, document.getElementById('root'))
+  <Provider store={store}>
+    <Router>
+      <Switch>
+        <Route path='/forum' component={Forum} />
+        <Route path='/' component={App} />
+      </Switch>
+    </Router>
+  </Provider>, document.getElementById('root'))
