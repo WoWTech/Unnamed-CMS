8dea770260816b2632536b2beed41a5f20f599e4
WoWTech <wow.tech.gen@gmail.com>
2018-03-09 02:54:19 +0200

    Implemented post comments selectors (reselect)
    Co-Authored-By: Volodymyr <dev.volodymyr@gmail.com>

--- resources/assets/js/selectors/index.js
+++ resources/assets/js/selectors/index.js
@@ -1,10 +1,34 @@
 import { createSelector } from 'reselect';
 
 const getPosts = state => state.entities.posts;
-const getIds = state => state.pagination.ids;
+const getPostsId = state => state.pagination.posts.ids;
+
+const getComments = state => state.entities.comments;
+const getCommentIds = (state, props) => {
+  const id = props.match.params.post;
+  const comments = state.pagination.comments[id];
+
+  return (comments && !comments.isFetching) ? comments.ids : [];
+}
+const getUsers = state => state.entities.accounts;
 
 export const getAllPosts = createSelector(
-  [ getPosts, getIds ], (posts, ids) => {
+  [ getPosts, getPostsId ], (posts, ids) => {
     return ids.map(id => posts[id]);
+  }
+)
+export const getPostComments = createSelector(
+  [ getComments, getCommentIds ], (comments, ids) => {
+    return ids.map(id => comments[id]);
+  }
+)
+
+export const getCommentsWithAuthors = createSelector(
+  [ getPostComments, getUsers ], (comments, users) => {
+    return comments.map(comment => {
+      const { username } = users[comment.account_id];
+
+      return Object.assign({}, comment, { username });
+    });
   }
 )
