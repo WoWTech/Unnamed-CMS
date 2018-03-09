947e8ba5053ea7822d5a46741331c26ddf5d2d73
WoWTech <wow.tech.gen@gmail.com>
2018-03-09 02:53:02 +0200

    Added default state for entities
    Co-Authored-By: Volodymyr <dev.volodymyr@gmail.com>

--- resources/assets/js/reducers/entities.js
+++ resources/assets/js/reducers/entities.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
 import merge from 'lodash/merge';
 
-const entities = (state = {}, action) => {
+const entities = (state = { posts: {}, comments: {}, accounts: {} }, action) => {
   if(action.response && action.response.entities) {
     return merge({}, state, action.response.entities);
   }
